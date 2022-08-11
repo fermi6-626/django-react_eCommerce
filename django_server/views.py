b6359f902691164540f7952faa276c89657432ef
Saksham <fermi4676.e@gmail.com>
2022-08-11 09:25:17 +0545

    -> Register(APIView) -> def post(self, request) [If password and confirmation doesn't match raise exception. Use serializers. If data is_valid, save the user. Return Response(serializer.data)]

--- django_server/views.py
+++ django_server/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import exceptions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import UserSerializer
 
 # Create your views here.
+
+
+class Register(APIView):
+    def post(self, request):
+        data = request.data
+
+        if data['password'] != data['pass_confirm']:
+            raise exceptions.APIException('Passwords do not match.')
+        
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
