b3ea4dd7737f38b9b1fbba28c421a6f6c2fd3ccd
Saksham <fermi4676.e@gmail.com>
2022-08-11 09:24:02 +0545

    -> Users(AbstractUser) -> USERNAME_FIELD = email [create class Users extending AbstractUser from django.contrib.auth.models. Create user's credentials]

--- django_server/models.py
+++ django_server/models.py
@@ -10,5 +10,5 @@ class Users(AbstractUser):
     password = models.CharField(max_length=255)
     username = models.CharField(max_length=25, unique=True)
     USERNAME_FIELD = 'username'
-    USERNAME_FIELD: 'email'
+    USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
