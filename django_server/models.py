2dbf49b567bd98e6779b8186e6c4ea37fa6b2d9c
Saksham <fermi4676.e@gmail.com>
2022-08-24 20:14:18 +0545

    -> Email_field='email'

--- django_server/models.py
+++ django_server/models.py
@@ -6,9 +6,9 @@ from django.db import models
 class Users(AbstractUser):
     first_name = models.CharField(max_length=25)
     last_name  = models.CharField(max_length=25)
-    email = models.CharField(max_length=25)
+    email = models.EmailField(max_length=25, unique=True)
     password = models.CharField(max_length=255)
     username = models.CharField(max_length=25, unique=True)
     USERNAME_FIELD = 'username'
-    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
