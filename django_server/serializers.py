1ff4a1fb260b81e5a9cdc94013f93cf1ff65f81c
Saksham <fermi4676.e@gmail.com>
2022-08-24 20:18:38 +0545

    -> line 20

--- django_server/serializers.py
+++ django_server/serializers.py
@@ -5,11 +5,17 @@ from .models import Users
 class UserSerializer(ModelSerializer):
     class Meta():
         model = Users
-        fields = ['id', 'first_name', 'last_name', 'email', 'password', ]
-        {
+        fields = ['id', 'first_name', 'last_name',
+                  'email', 'password', 'username']
+        extra_kwargs = {
             'password': {'write_only': True}
         }
 
-    def create(self, valid_data):
-        password = valid_data
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
 
