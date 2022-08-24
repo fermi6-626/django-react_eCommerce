0e7977b184a72b75ad38c012f393760472a43cfc
Saksham <fermi4676.e@gmail.com>
2022-08-24 20:20:13 +0545

    -> login(APIView), 24,49

--- django_server/views.py
+++ django_server/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from rest_framework import exceptions
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .serializers import UserSerializer
+from .models import Users
+from rest_framework import status
 
 # Create your views here.
 
@@ -12,9 +13,37 @@ class Register(APIView):
         data = request.data
 
         if data['password'] != data['pass_confirm']:
-            raise exceptions.APIException('Passwords do not match.')
-        
+            return Response('Passwords do not match.')
+
         serializer = UserSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
+        return Response(serializer.datai, status=status.HTTP_201_CREATED)
+
+
+class Login(APIView):
+    def post(self, request):
+        email = request.data.get("email")
+        username = request.data.get("username")
+        password = request.data.get("password")
+        err_stat = Response(
+            {"message": "invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+
+        login_email = Users.objects.get(email=email).first()
+        login_username = Users.objects.get(username=username).first()
+
+        if login_email:
+            if login_email is None:
+                return Response({"message": "invalid user"}, status=status.HTTP_404_NOT_FOUND)
+            if not login_email.check_password(password):
+                return err_stat
+            serializer = UserSerializer(login_email)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        if login_username:
+            if login_username is None:
+                return Response({"message": "invalid user"}, status=status.HTTP_404_NOT_FOUND)
+            if not login_username.check_password(password):
+                return err_stat
+            serializer = UserSerializer(login_username)
+            return Response(serializer.data, status=status.HTTP_200_OK)
