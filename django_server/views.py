ef541043c484e8916e49b67de6eff0793c3a6174
Saksham <fermi4676.e@gmail.com>
2022-08-25 10:37:37 +0545

    -> def query, else, 62

--- django_server/views.py
+++ django_server/views.py
@@ -18,7 +18,7 @@ class Register(APIView):
         serializer = UserSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.datai, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class Login(APIView):
@@ -26,24 +26,38 @@ class Login(APIView):
         email = request.data.get("email")
         username = request.data.get("username")
         password = request.data.get("password")
-        err_stat = Response(
-            {"message": "invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+        err_stat = Response({
+            "message": "invalid credentials"
+        }, status=status.HTTP_400_BAD_REQUEST)
 
-        login_email = Users.objects.get(email=email).first()
-        login_username = Users.objects.get(username=username).first()
-
-        if login_email:
-            if login_email is None:
+        def query(user):
+            if user is None:
                 return Response({"message": "invalid user"}, status=status.HTTP_404_NOT_FOUND)
-            if not login_email.check_password(password):
+            if not user.check_password(password):
                 return err_stat
-            serializer = UserSerializer(login_email)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+            serializer = UserSerializer(user)
+            return serializer
 
-        if login_username:
-            if login_username is None:
-                return Response({"message": "invalid user"}, status=status.HTTP_404_NOT_FOUND)
-            if not login_username.check_password(password):
-                return err_stat
-            serializer = UserSerializer(login_username)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+        if "email" in request.data and "username" not in request.data and "password" in request.data:
+            login_email = Users.objects.filter(email=email).first()
+            serializer = query(login_email)
+            return Response(query(login_email).data, status=status.HTTP_200_OK)
+            # if login_email is None:
+            #     return Response({"message": "invalid user"}, status=status.HTTP_404_NOT_FOUND)
+            # if not login_email.check_password(password):
+            #     return err_stat
+            # serializer = UserSerializer(login_email)
+            # return Response(serializer.data, status=status.HTTP_200_OK)
+
+        elif "username" in request.data and "email" not in request.data and "password" in request.data:
+            login_username = Users.objects.filter(username=username).first()
+            serializer = query(login_username)
+            return Response(query(login_username).data, status=status.HTTP_200_OK)
+            # if login_username is None:
+            #     return Response({"message": "invalid user"}, status=status.HTTP_404_NOT_FOUND)
+            # if not login_username.check_password(password):
+            #     return err_stat
+            # serializer = UserSerializer(login_username)
+            # return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "Request not understood"}, status=status.HTTP_400_BAD_REQUEST)
