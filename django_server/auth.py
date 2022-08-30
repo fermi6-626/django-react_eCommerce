64a3861a329cfb7c8826063b3fbe4cb2d8de6733
Saksham <fermi4676.e@gmail.com>
2022-08-30 23:30:21 +0545

    -> JWTAuthentication(+), authenticate(+)

--- django_server/auth.py
+++ django_server/auth.py
@@ -2,13 +2,27 @@ import jwt
 import datetime
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.authentication import BaseAuthentication, get_authorization_header
+from .models import Users
+
+
+class JWTAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        auth = get_authorization_header(request).split()
+
+        if auth and len(auth) == 2:
+            token = auth[1].decode('utf-8')
+            id = decode_access_token(token)
+            user = Users.objects.get(pk=id)
+            return user
+        return Response({"message": "Authentication error"}, status=status.HTTP_401_UNAUTHORIZED)
 
 
 def create_access_token(id):
     return jwt.encode({
         'user_id': id,
         'iat': datetime.datetime.utcnow(),
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30),
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
     }, 'access_secret', algorithm='HS256')
 
 
@@ -22,6 +36,6 @@ def create_refresh_token(id):
 
 def decode_access_token(token):
     try:
-        return jwt.decode(token, 'access_secret', algorithms='HS256').user_id
+        return jwt.decode(token, 'access_secret', algorithm='HS256').get('user_id')
     except:
         return Response({"message": "Authentication error"}, status=status.HTTP_401_UNAUTHORIZED)
