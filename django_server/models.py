4b5ccf136195762dd31f6ec75dc672ecc66f37c0
Saksham <fermi4676.e@gmail.com>
2022-09-01 21:41:06 +0545

    -> Tokens(models.Model), 18,22

--- django_server/models.py
+++ django_server/models.py
@@ -3,12 +3,20 @@ from django.db import models
 
 # Create your models here.
 
+
 class Users(AbstractUser):
     first_name = models.CharField(max_length=25)
-    last_name  = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
     email = models.EmailField(max_length=25, unique=True)
     password = models.CharField(max_length=255)
     username = models.CharField(max_length=25, unique=True)
     USERNAME_FIELD = 'username'
     EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
+
+
+class Tokens(models.Model):
+    user_id = models.IntegerField()
+    token = models.CharField(max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    expired = models.DateTimeField(None)
