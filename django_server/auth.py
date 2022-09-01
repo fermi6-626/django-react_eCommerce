d67ff4d151913cadcd1241c3867138ecf4d24cf3
Saksham <fermi4676.e@gmail.com>
2022-09-01 21:43:37 +0545

    -> 14, 19, 26, 38,42(~)

--- django_server/auth.py
+++ django_server/auth.py
@@ -11,19 +11,19 @@ class Auth(BaseAuthentication):
 
         if auth and len(auth) == 2:
             token = auth[1].decode('utf-8')
-            id = decode_token(token)
+            id = decode_token(token, 'access_token')
             user = Users.objects.get(pk=id)
             return (user, None)
 
         # rest_framework exception handler wtill convert it into appropriate Response with status_code
-        raise exceptions.AuthenticationFailed('Authentication Error')
+        raise exceptions.AuthenticationFailed('Authentication Error!')
 
 
 def create_token(id):
     return jwt.encode({
         'user_id': id,
         'iat': datetime.datetime.utcnow(),
-        'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=5),
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=30),
     }, 'access_token', algorithm='HS256')
 
 
@@ -35,8 +35,8 @@ def refresh_token(id):
     }, 'session_token', algorithm='HS256')
 
 
-def decode_token(token):
+def decode_token(token, which_token):
     try:
-        return jwt.decode(token, 'access_token', algorithms='HS256').get('user_id')
+        return jwt.decode(token, which_token, algorithms='HS256').get('user_id')
     except:
-        raise exceptions.AuthenticationFailed('Authentication Error')
+        raise exceptions.AuthenticationFailed('Authentication Error!')
