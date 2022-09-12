91237919388315492b9e7a0ee821398401a8469a
Saksham <fermi4676.e@gmail.com>
2022-09-12 09:35:54 +0545

    -> ForgotPasswd(~), ResetPassword(+)

--- django_server/views.py
+++ django_server/views.py
@@ -14,10 +14,8 @@ from django_server.auth import Auth, create_token, decode_token, refresh_token
 class Register(APIView):
     def post(self, request):
         data = request.data
-
         if data['password'] != data['pass_confirm']:
             return Response('Passwords do not match.')
-
         serializer = UserSerializer(data=data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
@@ -32,16 +30,13 @@ class Login(APIView):
         err_stat = Response({
             "message": "invalid credentials"
         }, status=status.HTTP_400_BAD_REQUEST)
-
         user = Users.objects.filter(email=email).first()
         if "username" in request.data:
             user = Users.objects.filter(username=username).first()
-
         if user is None:
             return Response({"message": "invalid user"}, status=status.HTTP_404_NOT_FOUND)
         if not user.check_password(password):
             return err_stat
-
         access_token = create_token(user.id)
         session_token = refresh_token(user.id)
         # when logging in Token in models.py is populated with following data
@@ -70,7 +65,6 @@ class RefreshSession(APIView):
     def post(self, request):
         refresh_token = request.COOKIES.get('session-token')
         id = decode_token(refresh_token, 'session_token')
-
         if not Tokens.objects.filter(
             user_id=id,
             token=refresh_token,
@@ -78,7 +72,6 @@ class RefreshSession(APIView):
         ).exists():
             raise exceptions.AuthenticationFailed('Authentication Error!')
         access_token = create_token(id)
-
         return Response({
             'token': access_token
         })
@@ -96,20 +89,43 @@ class Logout(APIView):
         return response
 
 
-class ResetPasswd(APIView):
+class ForgotPasswd(APIView):
     def post(self, request):
-        token = ''.join(random.choice(string.ascii_letters) for i in range(32))
         email = request.data.get('email')
+        token = ''.join(random.choice(string.ascii_letters) for i in range(32))
         Reset.objects.create(
             email=email,
             token=token
         )
+        url = "http://127.0.0.1:3000/reset/"+token
         send_mail(
             subject='Did you forget your password again?',
-            message='Click here to reset your password',
+            message='Click <a href="%s">here</a> to reset your password' % url,
             from_email='reset@email.com',
             recipient_list=[email],
         )
         return Response({
             'message': "Your password has been reset!"
         }, status=status.HTTP_200_OK)
+
+
+class ResetPasswd(APIView):
+    def post(self, request):
+        data = request.data
+        if data.get('password') != data.get('pass_confirm'):
+            return Response('Passwords do not match.')
+        reset_passwd = Reset.objects.filter(
+            token=data.get('token')
+        ).first()
+        if not reset_passwd:
+            raise exceptions.NotFound("Page doesn't exist!")
+        user = Users.objects.filter(
+            email=reset_passwd.email
+        ).first()
+        if not user:
+            raise exceptions.NotFound("User doesn't exist!")
+        user.set_password(data.get('password'))
+        user.save()
+        return Response({
+            "message": "Your new password is %s" % data.get('password')
+        })
